9cf22400c15e203059e8f665660cc8a06a50bce7
Kajal Sharma <divyansh_213042@saitm.org>
2025-07-13 22:40:21 +0530

    Initial commit with cleaned folders

---
+++ Day_18/Guessing_game.js
@@ -0,0 +1,29 @@
+const max = prompt("Enter the Max number: ");
+// console.log(max);
+
+const random = Math.floor(Math.random() * max) + 1;
+// console.log(random);
+
+let guess = prompt("guess the number : ");
+while (true) {
+        if (guess == "quit") {
+                console.log("user quit");
+                break;
+                
+        }
+        if(guess == random){
+                console.log("you are right. congrats, random number was "+ random);
+                break;
+        }else if (guess < random){
+               guess = prompt("your guess was too small . please try again.");
+        }else{
+                guess = prompt("your guess was too large. please try again.");
+        }
+        
+        // else{
+        //         guess = prompt("your guess was wrong. please try again.");
+        // }
+        
+}
+
+
---
+++ Day_18/app.js
@@ -0,0 +1,86 @@
+// 1. Objects Literals
+// USed to store keyed collections & complex entities.
+
+
+//         property => (key, value) pair
+
+//         Objects are a colllection of properties
+
+// Ex:--
+let student ={
+        name : "Divyansh",
+        age: 34,
+        class: "BTech",
+        Roll_num :213042 
+        
+};
+
+
+// Task 1
+// Thread / Twitter post 
+//         create an objects Literals for the properties of Thread/Twitter post which includes-
+
+//         username 
+//         content
+//         likes
+//         reposts
+//         tags
+
+const post ={
+        username : "@divyansh",
+        content : "THis is my first post",
+        likes : 200,
+        reposts : 15,
+        tags:["@saitm","mdu","hire", "SDE1"]
+};
+
+
+//add and update values
+// const student1 = {
+//         name:"divyansh",
+//         age:21,
+//         marks : 95,
+//         city : "Mumbai"
+// };
+
+
+// object of Objects
+// const classInfo = {
+//         aman : {
+//                 grade: "A+",
+//                 city:"Delhi"
+//         },
+//         Divyans:{
+//                 grade : "A",
+//                 city:"Gurgaon"
+//         },
+//         karan : {
+//                 grade : "B",
+//                 city : "DEoria"
+//         }
+// };
+
+
+const classInfo = [
+        {
+                name: "divyasnh",
+                grade:"A+",
+                city:"Delhi"
+
+        },
+        {
+                name:"yadav",
+                grade:"B+",
+                city:"MAu"
+        },
+        {
+                name:"Raju",
+                grade:"c",
+                city: "Gorkhpur"
+        }
+];
+
+
+//Random number generate 1 to 100
+
+// let random1 = Math.floor(Math.random()* 100) + 1;
