9abcfdb275fcd3726f5286aeab4d54f0f069b597
Kajal Sharma <divyansh_213042@saitm.org>
2025-07-22 23:30:38 +0530

    hi

--- Day_21/Part_8/App.js
+++ Day_21/Part_8/App.js
@@ -1,5 +1,5 @@
 //Array Methods
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 
 /* The code is using the `forEach` method on the `arr` array to iterate over each element in the array
 and log each element to the console. */
@@ -18,3 +18,20 @@ arr.forEach(function(el) {
 // };
 
 // arr.forEach(print);
+
+let arr = [{
+        name:"divy",
+        marks:95
+},
+{
+        name : "Rahul",
+        marks:85
+},
+{
+        name:"Dragon",
+        marks:75
+}];
+
+arr.forEach ((student) => {
+        console.log(student.marks);
+});
