5cab947d6ed9e05e0825d8416f2c4a704533d44a
Kajal Sharma <divyansh_213042@saitm.org>
2025-07-22 23:44:21 +0530

    map and filter funtion

--- Day_21/Part_8/App.js
+++ Day_21/Part_8/App.js
@@ -1,25 +1,59 @@
-//Array Methods
-// let arr = [1,2,3,4,5];
+// //Array Methods
+// // let arr = [1,2,3,4,5];
 
-/* The code is using the `forEach` method on the `arr` array to iterate over each element in the array
-and log each element to the console. */
-arr.forEach((el) => {
-        console.log(el);
-})
-
-
-arr.forEach(function(el) {
-        console.log(el);        
-});
-
-
-// let print = function (el){
+// /* The code is using the `forEach` method on the `arr` array to iterate over each element in the array
+// and log each element to the console. */
+// arr.forEach((el) => {
 //         console.log(el);
-// };
+// })
 
-// arr.forEach(print);
 
-let arr = [{
+// arr.forEach(function(el) {
+//         console.log(el);        
+// });
+
+
+// // let print = function (el){
+// //         console.log(el);
+// // };
+
+// // arr.forEach(print);
+
+// let arr = [{
+//         name:"divy",
+//         marks:95
+// },
+// {
+//         name : "Rahul",
+//         marks:85
+// },
+// {
+//         name:"Dragon",
+//         marks:75
+// }];
+
+// arr.forEach ((student) => {
+//         console.log(student.marks);
+// });
+
+
+
+
+// Map Function 
+// let newArr = arr.map(some function definition or name);
+
+
+// example
+
+// let num = [1,2,3,4];
+// let double = num.map((el) => {
+//         return el *el ;
+// });
+        
+
+
+
+let students = [{
         name:"divy",
         marks:95
 },
@@ -32,6 +66,16 @@ let arr = [{
         marks:75
 }];
 
-arr.forEach ((student) => {
-        console.log(student.marks);
+let gpa = students.map((el) =>{
+        return el.marks / 10;
 });
+
+
+
+// Filter function
+// let newArr = arr.filter(some function definition or name);
+
+let number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let ans = number.filter((el) =>{
+        return el % 2 == 0 ; //  even --> true , odd --> false
+})
