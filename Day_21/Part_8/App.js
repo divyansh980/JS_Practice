6c684c0ce865b3f6e0563cdc5677dd941834e1ac
Kajal Sharma <divyansh_213042@saitm.org>
2025-07-23 23:17:18 +0530

    hi

--- Day_21/Part_8/App.js
+++ Day_21/Part_8/App.js
@@ -79,3 +79,8 @@ let number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 let ans = number.filter((el) =>{
         return el % 2 == 0 ; //  even --> true , odd --> false
 })
+
+let number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let ans = number.filter((el) =>{
+        return el % 2 == 0 ; //  even --> true , odd --> false
+})
