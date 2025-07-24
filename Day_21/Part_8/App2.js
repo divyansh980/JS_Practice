51a5d90585f631db98a22335ca024a6e15dd21e2
Kajal Sharma <divyansh_213042@saitm.org>
2025-07-24 23:32:47 +0530

    reduce method

--- Day_21/Part_8/App2.js
+++ Day_21/Part_8/App2.js
@@ -16,3 +16,35 @@ true
 
 
 // Reduce method or function 
+
+let nums2 = [1,2,3,4];
+let finalVal = nums2.reduce((res, el) => res + el);
+console.log(finalVal);
+
+
+
+
+
+// find maximum number
+
+let arr = [1,4,2,5,6,7,9,2];
+
+// let max = -1;
+// for(let i = 0; i< arr.lenght; i++){
+//         if (max < arr[i]) {
+//                 max = arr[i];
+                
+//         }
+// }
+// console.log(max);
+
+// now using reduce menthod
+
+let max = arr.reduce((max, el ) =>{
+        if(max < el){
+                return el;
+        }else{
+                return max;
+        }
+});
+console.log(max);
