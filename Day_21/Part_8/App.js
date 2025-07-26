bb373653e1ae3d3d2a79690f43f217e9434722f2
Kajal Sharma <divyansh_213042@saitm.org>
2025-07-26 23:38:45 +0530

    Practice Qua

--- Day_21/Part_8/App.js
+++ Day_21/Part_8/App.js
@@ -1,82 +1,82 @@
-// //Array Methods
-// let arr = [1,2,3,4,5];
+// // //Array Methods
+// // let arr = [1,2,3,4,5];
 
-/* The code is using the `forEach` method on the `arr` array to iterate over each element in the array
-and log each element to the console. */
-arr.forEach((el) => {
-        console.log(el);
-})
-
-
-arr.forEach(function(el) {
-        console.log(el);        
-});
-
-
-// let print = function (el){
+// /* The code is using the `forEach` method on the `arr` array to iterate over each element in the array
+// and log each element to the console. */
+// arr.forEach((el) => {
 //         console.log(el);
-// };
-
-// arr.forEach(print);
-
-let arr = [{
-        name:"divy",
-        marks:95
-},
-{
-        name : "Rahul",
-        marks:85
-},
-{
-        name:"Dragon",
-        marks:75
-}];
-
-arr.forEach ((student) => {
-        console.log(student.marks);
-});
+// })
 
 
+// arr.forEach(function(el) {
+//         console.log(el);        
+// });
 
 
-// Map Function 
-// let newArr = arr.map(some function definition or name);
+// // let print = function (el){
+// //         console.log(el);
+// // };
+
+// // arr.forEach(print);
+
+// let arr = [{
+//         name:"divy",
+//         marks:95
+// },
+// {
+//         name : "Rahul",
+//         marks:85
+// },
+// {
+//         name:"Dragon",
+//         marks:75
+// }];
+
+// arr.forEach ((student) => {
+//         console.log(student.marks);
+// });
 
 
-// example
 
-let num = [1,2,3,4];
-let double = num.map((el) => {
-        return el *el ;
-});
+
+// // Map Function 
+// // let newArr = arr.map(some function definition or name);
+
+
+// // example
+
+// let num = [1,2,3,4];
+// let double = num.map((el) => {
+//         return el *el ;
+// });
         
 
 
 
-let students = [{
-        name:"divy",
-        marks:95
-},
-{
-        name : "Rahul",
-        marks:85
-},
-{
-        name:"Dragon",
-        marks:75
-}];
+// let students = [{
+//         name:"divy",
+//         marks:95
+// },
+// {
+//         name : "Rahul",
+//         marks:85
+// },
+// {
+//         name:"Dragon",
+//         marks:75
+// }];
 
-let gpa = students.map((el) =>{
-        return el.marks / 10;
-});
+// let gpa = students.map((el) =>{
+//         return el.marks / 10;
+// });
 
 
 
-// Filter function
-// let newArr = arr.filter(some function definition or name);
+// // Filter function
+// // let newArr = arr.filter(some function definition or name);
 
-let number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-let ans = number.filter((el) =>{
-        return el % 2 == 0 ; //  even --> true , odd --> false
-})
+// let number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// let ans = number.filter((el) =>{
+//         return el % 2 == 0 ; //  even --> true , odd --> false
+// })
 
--- Day_21/Part_8/App2.js
+++ Day_21/Part_8/App2.js
@@ -1,66 +1,98 @@
-// [2,4,6].every((el) => el%2 == 0);
-// true
+// // [2,4,6].every((el) => el%2 == 0);
+// // true
 
-// [2,4,6,8].every((el) => el%2 == 0);
-// true
+// // [2,4,6,8].every((el) => el%2 == 0);
+// // true
 
-// [2,4,6,8,1].every((el) => el%2 == 0);
-// false
+// // [2,4,6,8,1].every((el) => el%2 == 0);
+// // false
 
-// [3,5,7,9,11].every((el) => el%2 == 0);
-// false
+// // [3,5,7,9,11].every((el) => el%2 == 0);
+// // false
 
-// [3,5,7,9,11].every((el) => el%2 != 0);
-// true
-
-
-
-// Reduce method or function 
-
-// let nums2 = [1,2,3,4];
-// let finalVal = nums2.reduce((res, el) => res + el);
-// console.log(finalVal);
+// // [3,5,7,9,11].every((el) => el%2 != 0);
+// // true
 
 
 
+// // Reduce method or function 
+
+// // let nums2 = [1,2,3,4];
+// // let finalVal = nums2.reduce((res, el) => res + el);
+// // console.log(finalVal);
 
 
-// find maximum number
 
-// let arr = [1,4,2,5,6,7,9,2];
 
-// let max = -1;
-// for(let i = 0; i< arr.lenght; i++){
-//         if (max < arr[i]) {
-//                 max = arr[i];
+
+// // find maximum number
+
+// // let arr = [1,4,2,5,6,7,9,2];
+
+// // let max = -1;
+// // for(let i = 0; i< arr.lenght; i++){
+// //         if (max < arr[i]) {
+// //                 max = arr[i];
                 
-//         }
-// }
-// console.log(max);
+// //         }
+// // }
+// // console.log(max);
 
-// now using reduce menthod
+// // now using reduce menthod
 
-// let max = arr.reduce((max, el ) =>{
-//         if(max < el){
+// // let max = arr.reduce((max, el ) =>{
+// //         if(max < el){
+// //                 return el;
+// //         }else{
+// //                 return max;
+// //         }
+// // });
+// // console.log(max);
+
+
+
+
+// // Que: Finding Maximum in an array
+
+// let max = arr.reduce((max, el) => {
+//         if(max <el){
 //                 return el;
 //         }else{
 //                 return max;
 //         }
 // });
-// console.log(max);
+// console.log(max); 
+
+//   let a = 3;
+//   console.log(a);  
 
 
 
 
-// Que: Finding Maximum in an array
 
-let max = arr.reduce((max, el) => {
-        if(max <el){
-                return el;
-        }else{
-                return max;
-        }
-});
-console.log(max); 
+// Practice Ques1
+// Check if all numbers in our array are multiple fo 10 or not.
 
-    
+let nums = [10,20,30,40];
+
+let ans = nums.every((el) => el %10 == 0 );
+console.log(ans);
+
+
+
+
+
+
+function getMin(nums2){
+        let min = nums2.reduce((min, el) =>{
+                if(min>el){
+                        return min;
+
+                }else{
+                        return el;
+                }
+        });
+        return min;
+}
+
+let nums2 = [10,20,30,40,5]
