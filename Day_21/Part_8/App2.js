284bcc2d9d08ec5e2287fd5b42a9a6bde2204f82
Kajal Sharma <divyansh_213042@saitm.org>
2025-07-26 23:20:44 +0530

    reduce function

--- Day_21/Part_8/App2.js
+++ Day_21/Part_8/App2.js
@@ -1,25 +1,25 @@
-[2,4,6].every((el) => el%2 == 0);
-true
+// [2,4,6].every((el) => el%2 == 0);
+// true
 
-[2,4,6,8].every((el) => el%2 == 0);
-true
+// [2,4,6,8].every((el) => el%2 == 0);
+// true
 
-[2,4,6,8,1].every((el) => el%2 == 0);
-false
+// [2,4,6,8,1].every((el) => el%2 == 0);
+// false
 
-[3,5,7,9,11].every((el) => el%2 == 0);
-false
+// [3,5,7,9,11].every((el) => el%2 == 0);
+// false
 
-[3,5,7,9,11].every((el) => el%2 != 0);
-true
+// [3,5,7,9,11].every((el) => el%2 != 0);
+// true
 
 
 
 // Reduce method or function 
 
-let nums2 = [1,2,3,4];
-let finalVal = nums2.reduce((res, el) => res + el);
-console.log(finalVal);
+// let nums2 = [1,2,3,4];
+// let finalVal = nums2.reduce((res, el) => res + el);
+// console.log(finalVal);
 
 
 
@@ -27,7 +27,7 @@ console.log(finalVal);
 
 // find maximum number
 
-let arr = [1,4,2,5,6,7,9,2];
+// let arr = [1,4,2,5,6,7,9,2];
 
 // let max = -1;
 // for(let i = 0; i< arr.lenght; i++){
@@ -40,11 +40,27 @@ let arr = [1,4,2,5,6,7,9,2];
 
 // now using reduce menthod
 
-let max = arr.reduce((max, el ) =>{
-        if(max < el){
+// let max = arr.reduce((max, el ) =>{
+//         if(max < el){
+//                 return el;
+//         }else{
+//                 return max;
+//         }
+// });
+// console.log(max);
+
+
+
+
+// Que: Finding Maximum in an array
+
+let max = arr.reduce((max, el) => {
+        if(max <el){
                 return el;
         }else{
                 return max;
         }
 });
-console.log(max);
+console.log(max); 
+
+    
